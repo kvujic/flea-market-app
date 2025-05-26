47eec2304a095a2f32b0d12e135e6e1ebf6b931d
kaorivujic <kiyomaru.56129@gmail.com>
2025-05-26 15:27:05 +0900

    add tables

---
+++ src/database/migrations/2025_05_25_165135_create_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('description', 255);
+            $table->string('brand')->nullable();
+            $table->decimal('price', 10, 2)->unsigned();
+            $table->foreignId('condition_id')->constrained()->cascadeOnDelete();
+            $table->string('item_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
---
+++ src/database/migrations/2025_05_25_165309_create_purchases_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('item_id')->constrained()->cascadeOnDelete();
+            $table->string('payment_method');
+            $table->decimal('amount', 10, 2);
+            $table->string('shipping_postal_code', 8);
+            $table->string('shipping_address');
+            $table->string('shipping_building')->nullable();
+            $table->timestamp('payment_date')->nullable();
+            $table->string('stripe_transaction_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
